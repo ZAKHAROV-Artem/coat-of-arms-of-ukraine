3a25cd4ea9f143d7d031cc0db683b9d530298c0b
ZAKHAROV-Artem <szakharovartem@gmail.com>
2024-04-28 23:01:13 +0200

    ci: added env variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,19 @@ export default function Home() {
       <Intro />
       <div className="flex flex-wrap justify-center gap-2 p-2 sm:gap-3 sm:p-3 md:gap-5 md:p-5">
         <Trident
-          link="http://localhost:3000/api/v1/tridents/official?fill=fff&size=1"
+          link={`${process.env.NEXT_PUBLIC_URL}/api/v1/tridents/official?fill=fff&size=1`}
           width={276}
           height={400}
           name="Offial"
         />
         <Trident
-          link="http://localhost:3000/api/v1/tridents/pixel?fill=fff&size=1"
+          link={`${process.env.NEXT_PUBLIC_URL}/api/v1/tridents/pixel?fill=fff&size=1`}
           width={276}
           height={400}
           name="Pixel"
         />
         <Trident
-          link="http://localhost:3000/api/v1/tridents/simple?fill=fff&size=1"
+          link={`${process.env.NEXT_PUBLIC_URL}/api/v1/tridents/simple?fill=fff&size=1`}
           width={276}
           height={400}
           name="Simple"
