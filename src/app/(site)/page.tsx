37e0406c5781b3af40966f246e173af80a7c3138
ZAKHAROV-Artem <szakharovartem@gmail.com>
2024-05-03 11:02:45 +0200

    refactor: changed props to Trident component and added link to trident' details page

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -9,13 +9,7 @@ export default async function Home() {
     <>
       <div className="flex flex-wrap justify-center gap-2 p-2 sm:gap-3 sm:p-3 md:gap-5 md:p-5">
         {tridents.map((trident) => (
-          <Trident
-            key={trident._id}
-            link={`${process.env.NEXT_PUBLIC_URL}/api/v1/tridents/${trident.api.current}?fill=fff&size=1`}
-            width={trident.width}
-            height={trident.height}
-            name={trident.name}
-          />
+          <Trident key={trident._id} trident={trident} />
         ))}
       </div>
     </>
--- src/components/trident/trident.tsx
+++ src/components/trident/trident.tsx
@@ -1,19 +1,20 @@
 import { sliceText } from "@/lib/utils";
+import { Trident as TridentType } from "@/types/sanity";
 import Image from "next/image";
+import Link from "next/link";
 type Props = {
-  name: string;
-  link: string;
-  width: number;
-  height: number;
+  trident: TridentType;
 };
-export default function Trident({ name, link, width, height }: Props) {
+export default function Trident({
+  trident: { name, api, width, height },
+}: Props) {
   return (
     <div className="flex max-w-[290px] grow flex-col justify-between rounded-2xl border border-stone-700 sm:grow-0">
       <div className="mx-auto">
         <div className="flex h-[150px] justify-center overflow-hidden p-3 sm:h-[290px] sm:w-[290px] sm:p-5">
           <Image
             className="h-full w-full object-contain"
-            src={link}
+            src={`${process.env.NEXT_PUBLIC_URL}/api/v1/tridents/${api.current}?fill=fff&size=1`}
             alt={name}
             width={width}
             height={height}
@@ -22,9 +23,12 @@ export default function Trident({ name, link, width, height }: Props) {
       </div>
       <div className="flex grow flex-col justify-between p-3 sm:p-5">
         <h2 className="break-words text-2xl">{sliceText(name, 30)}</h2>
-        <button className="mt-3 rounded-md bg-wheat p-1 text-black sm:mt-5 sm:px-3 sm:py-2">
+        <Link
+          href={`${api.current}`}
+          className="mt-3 rounded-md bg-wheat p-1 text-center text-black sm:mt-5 sm:px-3 sm:py-2"
+        >
           Налаштувати
-        </button>
+        </Link>
       </div>
     </div>
   );
