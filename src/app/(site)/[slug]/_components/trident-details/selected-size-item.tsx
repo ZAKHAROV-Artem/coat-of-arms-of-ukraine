7efef4727a28918066c773023155c57215f0a874
Paul-Hammer <65820985+Paul-Hammer@users.noreply.github.com>
2024-05-30 14:25:28 +0300

    Update selected-size-item.tsx
    Refactore SelectedSizeItem component.

--- src/app/(site)/[slug]/_components/trident-details/selected-size-item.tsx
+++ src/app/(site)/[slug]/_components/trident-details/selected-size-item.tsx
@@ -5,6 +5,7 @@ type Props = {
   size: number;
   setSize: (size: number) => void;
 };
+
 export default function SelectedSizeItem({
   selectedSize,
   size,
@@ -22,3 +23,19 @@ export default function SelectedSizeItem({
     </button>
   );
 }
+
+// I guess this component should work with those props instead.
+type Props = React.PropsWithChildren & {
+  isSelected: boolean; // size === selectedSize
+  setSize: () => void; // You already know "size" outside - no reason to pass it inside
+}
+
+// In this case this component is more generic. It is now basically a presentation. You can pass any callback here and any text.
+export default const SelectedSizeItem = ({ isActive, children, setSize }: Props) => (
+  <button
+    className={cn("grid h-10 w-10 place-content-center rounded-full border border-wheat text-white duration-200", isSelected && "bg-wheat text-black")}
+    onClick={setSize}
+  >
+    {children}
+  </button>
+);
